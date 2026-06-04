f29a26caf51d2b919724bba62f00544119e807e9
silver3010 <banuelos.karen.2024@cbtis-246.edu.mx>
2026-06-04 01:41:27 -0600

    Add files via upload

---
+++ tres.py
@@ -0,0 +1,13 @@
+class department
+    def twosun(self, nums: List[int], employed: int) -> List[int])
+
+    public int id = id
+    public varchar employee = employee
+    public int salary = salary
+    public varchar dapartment = department
+
+    public string obtener resultado()
+    return ${'id'} {'department'},{'salary'},{'employee'}
+    
+    
+    pass
