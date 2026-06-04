c3f46c5989947ef83e7642928219de9427bf249b
silver3010 <banuelos.karen.2024@cbtis-246.edu.mx>
2026-06-04 01:40:45 -0600

    Add files via upload

---
+++ uno.py
@@ -0,0 +1,12 @@
+class customer import user
+
+def activity(self):
+    customer = int
+    product = int
+
+public int customer = customer
+    public = int 
+    publoc = int
+
+    public string obtener resultado()
+    return ${'customer'} {'product'}
