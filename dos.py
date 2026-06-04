5492c4b2050b1d8c13079f64caddd88be08090b3
silver3010 <banuelos.karen.2024@cbtis-246.edu.mx>
2026-06-04 01:41:09 -0600

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,12 @@
+class public 
+def trips(db.model):
+    id = db.column(integer, primary-key)
+    client = db.column(nullable=false)
+    driver = db.column(nullable=false)
+    city = db.column(nullable=false)
+
+def users(db.model)
+    users_id = db.column(integer, primary-key)
+    banned_id = db.column(nullabl=false)
+    role = db.column(nullable=false)
+
