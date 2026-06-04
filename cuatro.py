0090975a5ec391e5979e7459854bfeb53cbb3d36
silver3010 <banuelos.karen.2024@cbtis-246.edu.mx>
2026-06-04 01:41:46 -0600

    Add files via upload

---
+++ cuatro.py
@@ -0,0 +1,17 @@
+class player import user
+
+def activity(self):
+    player = int
+    device = int
+    event_date = date
+    games_played = int
+
+public int player = player
+    public = int 
+    publoc = int
+    public = date
+    public = int
+
+    public string obtener resultado()
+    return ${'player'} {'device'},{'event'},{'games_played'}
+    
